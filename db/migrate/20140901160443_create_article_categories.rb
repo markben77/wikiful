84e068ee41f33926c57f43344836961bef2ab186
markben77 <markbenlarson@gmail.com>
2014-09-01 09:41:20 -0700

    models updated

--- db/migrate/20140901160443_create_article_categories.rb
+++ db/migrate/20140901160443_create_article_categories.rb
@@ -1,7 +1,8 @@
 class CreateArticleCategories < ActiveRecord::Migration
   def change
     create_table :article_categories do |t|
-
+    	t.belongs_to :category
+    	t.belongs_to :article
       t.timestamps
     end
   end
