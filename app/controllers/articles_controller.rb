9c4213804cf36093654c5473da833dbe8ab98338
markben77 <markbenlarson@gmail.com>
2014-09-07 16:17:05 -0700

    Index view updated, methods to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,11 @@ class ArticlesController < ApplicationController
 
 	def index
 
+		@articles = Article.order(updated_at: :desc).limit(25)
 	end
 
 	def show
-
+		@article = Article.find(params[:id])
 	end
 
 	def new
