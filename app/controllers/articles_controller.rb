1a993635117a163102a426b2d6068e335308831a
markben77 <markbenlarson@gmail.com>
2014-09-08 08:21:56 -0700

    Show page finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
 
 	def index
-
-		@articles = Article.order(updated_at: :desc).limit(25)
+		@articles = Article.order({title: :asc, updated_at: :desc}).limit(25)
 	end
 
 	def show
