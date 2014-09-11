cbe28acbe61219250960f60f0995061daa5ec0c5
markben77 <markbenlarson@gmail.com>
2014-09-11 07:22:52 -0700

    basic crud operations working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,36 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
+	end
 
+	def edit
+		@article = Article.find(params[:id])	
 	end
 
 	def create
-
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render "new"
+		end
 	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render "update", flash: "error of some kind"
+		end
+	end
+
+	private
+
+		def article_params
+			params.require(:article).permit(:title, :content, :category_ids => [])
+		end
 
 end
