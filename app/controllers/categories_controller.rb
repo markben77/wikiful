70eef23b6a22fd3102d413f05b48011f9cc032f0
markben77 <markbenlarson@gmail.com>
2014-09-12 07:39:49 -0700

    Broken code sharing with Alvin

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+	
+	def index
+		@categories = Category.order(name: :asc)
+	end
+
+	def show
+		@category = Category.find(params[:id])
+		@articles = Article.where(category_id: params[:id])
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def edit
+		@category = Category.find(params[:id])
+		@articles = Article.where(category_id: params[:id])
+	end
+
+
+	def create
+		@category = Category.new(category_params)
+		if (@category.save)
+			redirect_to @category
+		else
+			render "new"
+		end
+
+	end
+
+	private
+
+		def category_params
+			params.require(:category).permit(:id, :name)
+		end
+end
